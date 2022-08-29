1923f501b86e276b6859d6e28dbd3a9355bdbe47
Maigol <98134114+Maigol193@users.noreply.github.com>
2022-08-29 15:20:09 -0500

    Add files via upload

--- app/app.c
+++ app/app.c
@@ -2,11 +2,5 @@
 #include "stdio.h"
 
 int main(void){
-    int test[] = {0,9,18,-65,-90,0,0,1};
-    int swaps = bubble_sort(test,8);
-    for(int i = 0; i < 8; i++){
-        printf("%d ", test[i]);
-    }
-    printf("\nNumero de swaps %d ", swaps);
     return 0;
 }
